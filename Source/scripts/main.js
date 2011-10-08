20a50dc2d3562560597428bb9cf9143d89ff3495
Carter Allen <CarterA@opt-6.com>
2011-10-08 17:00:14 -0600

    Switched over to Google Analytics.

--- Source/scripts/main.js
+++ Source/scripts/main.js
@@ -1,2 +1,8 @@
-function loadMint(){var head=document.getElementsByTagName('head')[0];var script=document.createElement('script');script.type='text/javascript';script.src='/mint/?js';head.appendChild(script);}window.onload=loadMint;
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-26206485-1']);
+_gaq.push(['_trackPageview']);
+(function(){var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
 (function(){if(!/*@cc_on!@*/0)return;var e="abbr,article,aside,audio,bb,canvas,datagrid,datalist,details,dialog,eventsource,figure,footer,header,hgroup,mark,menu,meter,nav,output,progress,section,time,video".split(',');for(var i=0;i<e.length;i++){document.createElement(e[i]);}})();
